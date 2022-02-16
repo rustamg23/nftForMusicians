e9a894de3d0bb85ac5e7e4be4476d76b04dab33a
rustamg23 <rustamg23@outlook.com>
2022-02-16 20:28:05 +0400

    alpha 0.0.2

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const multer = require('multer');
 const path = require('path');
-const app1 = express();
+const app = express();
 const helpers = require('./helpers');
+
+const getProvider = () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        return provider;
+      }
+    }
+    window.open("https://phantom.app/", "_blank");
+  };
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, 'uploads/');
     },
-
-    // By default, multer removes file extensions so let's add them back
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
-// создаем парсер для данных application/x-www-form-urlencoded
 const urlencodedParser = express.urlencoded({extended: false});
   
-app1.get("/", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app1.post('/upload-profile-pic', urlencodedParser, function (req, res) {
+
+
+
+app.post('/upload-profile-pic', urlencodedParser, function (req, res) {
     if(!req.body) return res.sendStatus(400);
     let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
-    console.log(req.body.userName);
+    
     upload(req, res, function(err) {
         if (req.fileValidationError) {
             return res.send(req.fileValidationError);
@@ -37,11 +48,12 @@ app1.post('/upload-profile-pic', urlencodedParser, function (req, res) {
             return res.send(err);
         }
 
-        res.send(`You have uploaded image ${req.file.path}`);
+        res.send(`You have uploaded image`);// ${req.file.path} <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">`);
+        console.log(req.body.userName);
     });
     
     
     //res.send(`${req.body.userName} - ${req.body.userImage}`);
 });
    
-app1.listen(3000, ()=>console.log("Сервер запущен..."));
+app.listen(3000, ()=>console.log("Сервер запущен..."));
